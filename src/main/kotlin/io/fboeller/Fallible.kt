7626380f8c90d0e53d796f641ba47f259eda5604
fboeller <fabianboeller@googlemail.com>
2020-02-28 23:59:51 +0100

    Extract Fallible class

---
+++ src/main/kotlin/io/fboeller/Fallible.kt
@@ -0,0 +1,27 @@
+package io.fboeller
+
+sealed class Fallible<T>
+data class Success<T>(val t: T) : Fallible<T>()
+data class Failure<T>(val reasons: List<String>): Fallible<T>()
+
+fun <T, R> Fallible<T>.flatMap(f: (T) -> Fallible<R>): Fallible<R> = when (this) {
+    is Success -> f(this.t)
+    is Failure -> Failure(this.reasons)
+}
+
+fun <T, R> Fallible<T>.map(f: (T) -> R): Fallible<R> =
+    flatMap { Success(f(it)) }
+
+fun <T> merge(fallible1: Fallible<List<T>>, fallible2: Fallible<List<T>>): Fallible<List<T>> = when(fallible1) {
+    is Success -> when(fallible2) {
+        is Success -> Success(fallible1.t.union(fallible2.t).toList())
+        is Failure -> Failure(fallible2.reasons)
+    }
+    is Failure -> when(fallible2) {
+        is Success -> Failure(fallible1.reasons)
+        is Failure -> Failure(fallible1.reasons.union(fallible2.reasons).toList())
+    }
+}
+
+fun <T> sequence(fallibles: List<Fallible<T>>): Fallible<List<T>> =
+    fallibles.fold(Success(listOf()), { acc, fallible -> merge(acc, fallible.map { listOf(it) }) })
--- src/main/kotlin/io/fboeller/ParserDSLSafe.kt
+++ src/main/kotlin/io/fboeller/ParserDSLSafe.kt
@@ -1,17 +1,5 @@
 package io.fboeller
 
-sealed class Fallible<T>
-data class Success<T>(val t: T) : Fallible<T>()
-data class Failure<T>(val reasons: List<String>): Fallible<T>()
-
-fun <T, R> Fallible<T>.flatMap(f: (T) -> Fallible<R>): Fallible<R> = when (this) {
-    is Success -> f(this.t)
-    is Failure -> Failure(this.reasons)
-}
-
-fun <T, R> Fallible<T>.map(f: (T) -> R): Fallible<R> =
-    flatMap { Success(f(it)) }
-
 typealias FallibleParser<T> = (Json) -> Fallible<T>
 typealias FallibleOParser<T> = (JsonObject) -> Fallible<T>
 typealias FallibleLParser<T> = (JsonList) -> Fallible<T>
@@ -45,20 +33,6 @@ class ParserDSLSafe {
         fun <T> field(field: String, parse: (Json?) -> Fallible<T>): FallibleOParser<T> = { json ->
             parse(json.fields[field])
         }
-
-        fun <T> merge(fallible1: Fallible<List<T>>, fallible2: Fallible<List<T>>): Fallible<List<T>> = when(fallible1) {
-            is Success -> when(fallible2) {
-                is Success -> Success(fallible1.t.union(fallible2.t).toList())
-                is Failure -> Failure(fallible2.reasons)
-            }
-            is Failure -> when(fallible2) {
-                is Success -> Failure(fallible1.reasons)
-                is Failure -> Failure(fallible1.reasons.union(fallible2.reasons).toList())
-            }
-        }
-
-        fun <T> sequence(fallibles: List<Fallible<T>>): Fallible<List<T>> =
-            fallibles.fold(Success(listOf()), { acc, fallible -> merge(acc, fallible.map { listOf(it) }) })
 
 
         val string: FallibleParser<String> =
