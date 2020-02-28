fee4b357c5bc6da413b3dbd63bc649a1c9fe3fe0
fboeller <fabianboeller@googlemail.com>
2020-02-28 20:48:38 +0100

    Add lifts

--- src/main/kotlin/io/fboeller/JsonTypes.kt
+++ src/main/kotlin/io/fboeller/JsonTypes.kt
@@ -9,6 +9,8 @@ data class JsonPrimitive(val value: String) : Json()
 
 typealias Parser<T> = (Json) -> T
 typealias OParser<T> = (JsonObject) -> T
+typealias LParser<T> = (JsonList) -> T
+typealias PParser<T> = (JsonPrimitive) -> T
 
 fun <T> fold(fo: (JsonObject) -> T, fl: (JsonList) -> T, fp: (JsonPrimitive) -> T): Parser<T> = { json ->
     when (json) {
@@ -18,21 +20,29 @@ fun <T> fold(fo: (JsonObject) -> T, fl: (JsonList) -> T, fp: (JsonPrimitive) -> 
     }
 }
 
-fun <T> expectList(parse: (JsonList) -> T): Parser<T> =
+fun <T> expectList(parse: LParser<T>): Parser<T> =
     fold({ fail() }, parse, { fail() })
 
-fun <T> expectObject(parse: (JsonObject) -> T): Parser<T> =
+fun <T> expectObject(parse: OParser<T>): Parser<T> =
     fold(parse, { fail() }, { fail() })
 
-fun <T> expectString(parse: (JsonPrimitive) -> T): Parser<T> =
+fun <T> expectString(parse: PParser<T>): Parser<T> =
     fold({ fail() }, { fail() }, parse)
 
 fun <T> expectField(field: String, parse: (Json?) -> T): OParser<T> = { json ->
     parse(json.fields[field])
 }
 
-fun <T1, T2, R> lift(f: (T1, T2) -> R): (T1?, T2?) -> R? = { t1, t2 ->
+fun <T1, T2, R> liftOption(f: (T1, T2) -> R): (T1?, T2?) -> R? = { t1, t2 ->
     t1?.let { s1 -> t2?.let { s2 -> f(s1, s2) } }
+}
+
+fun <T1, T2, R> liftParser(f: (T1, T2) -> R): (Parser<T1>, Parser<T2>) -> Parser<R> = { p1, p2 ->
+    { json -> f(p1(json), p2(json)) }
+}
+
+fun <T1, T2, R> liftOParser(f: (T1, T2) -> R): (OParser<T1>, OParser<T2>) -> OParser<R> = { p1, p2 ->
+    { json -> f(p1(json), p2(json)) }
 }
 
 val myJson: Json = JsonObject(
@@ -46,14 +56,14 @@ data class Person(val name: String, val hobbies: List<String>)
 
 fun fail(): Nothing = throw RuntimeException()
 
-fun parseJson(json: Json): Person? =
-    expectObject(::parsePerson)(json)
-
-fun parsePerson(o: JsonObject): Person? =
-    lift(::Person)(
-        expectField("name", ::parseName)(o),
-        expectField("hobbies", ::parseHobbies)(o)
+val parsePerson: OParser<Person?> =
+    liftOParser(liftOption(::Person))(
+        expectField("name", ::parseName),
+        expectField("hobbies", ::parseHobbies)
     )
+
+val parseJson: Parser<Person?> =
+    expectObject(parsePerson)
 
 fun parseName(maybeJson: Json?): String? =
     maybeJson?.let(expectString(JsonPrimitive::value))
