d4e375e241c4d39522e7109cd7ddac84c33d9d3f
fboeller <fabianboeller@googlemail.com>
2020-02-29 22:43:01 +0100

    Systematically approach tests

--- src/test/kotlin/io/fboeller/ParserDSLTest.kt
+++ src/test/kotlin/io/fboeller/ParserDSLTest.kt
@@ -1,90 +1,96 @@
 package io.fboeller
 
-import io.fboeller.ParserDSL.Companion.field
 import io.fboeller.ParserDSL.Companion.fieldsOf
 import io.fboeller.ParserDSL.Companion.string
-import io.fboeller.ParserDSL.Companion.listOf
+import io.fboeller.ParserDSL.Companion.list
+import io.fboeller.ParserDSL.Companion.field
 import io.fboeller.ParserDSL.Companion.mandatory
 import io.fboeller.ParserDSL.Companion.obj
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class ParserDSLTest {
+class ParserDSLTest : StringSpec({
+
+    "String parser succeeds" {
+        forAll(
+            row(JsonPrimitive("str"), "str")
+        ) { json, result ->
+            string(json) shouldBe Success(result)
+        }
+    }
+
+    "String parser fails" {
+        forAll(
+            row(JsonList(emptyList()), listOf("<root> is not a string but a list")),
+            row(JsonObj(emptyMap()), listOf("<root> is not a string but an object"))
+        ) { json, result ->
+            string(json).reasons().map { it.print() } shouldBe result
+        }
+    }
+
+    "List parser succeeds" {
+        forAll(
+            row(JsonList(emptyList()), emptyList()),
+            row(JsonList(listOf(JsonPrimitive("str"))), listOf("str")),
+            row(JsonList(listOf(JsonPrimitive("str1"), JsonPrimitive("str2"))), listOf("str1", "str2"))
+        ) { json, result ->
+            string.list()(json) shouldBe Success(result)
+        }
+    }
+
+    "List parser fails" {
+        forAll(
+            row(JsonPrimitive("str"), listOf("<root> is not a list but a string")),
+            row(JsonList(listOf(JsonList(emptyList()))), listOf("<root>[0] is not a string but a list")),
+            row(JsonList(listOf(JsonObj(emptyMap()))), listOf("<root>[0] is not a string but an object")),
+            row(
+                JsonList(listOf(JsonList(emptyList()), JsonPrimitive("str"), JsonObj(emptyMap()))),
+                listOf("<root>[0] is not a string but a list", "<root>[2] is not a string but an object")
+            )
+        ) { json, result ->
+            string.list()(json).reasons().map { it.print() } shouldBe result
+        }
+    }
 
     data class Person(
-        val name: String,
-        val surname: String?,
-        val hobbies: List<String>
+        val firstName: String,
+        val lastName: String?
     )
 
-    val personOf: Parser<Result<Person>> = obj(
+    val personOf = obj(
         fieldsOf(::Person)(
-            string.field("name").mandatory(),
-            string.field("surname"),
-            listOf(string).field("hobbies").mandatory()
+            string.field("firstName").mandatory(),
+            string.field("lastName")
         )
     )
 
-    @Test
-    fun `JSON structure with all fields is parsed correctly`() {
-        val myJson: Json = JsonObj(
-            mapOf(
-                "name" to JsonPrimitive("b"),
-                "surname" to JsonPrimitive("c"),
-                "hobbies" to JsonList(listOf(JsonPrimitive("d")))
+    "Object parser succeeds" {
+        forAll(
+            row(JsonObj(mapOf("firstName" to JsonPrimitive("Heinz"))), Person("Heinz", null)),
+            row(
+                JsonObj(mapOf("firstName" to JsonPrimitive("Heinz"), "lastName" to JsonPrimitive("Schmidt"))),
+                Person("Heinz", "Schmidt")
+            ),
+            row(
+                JsonObj(mapOf("firstName" to JsonPrimitive("Heinz"), "otherField" to JsonPrimitive("Schmidt"))),
+                Person("Heinz", null)
             )
-        )
-        Assertions.assertThat(personOf(myJson))
-            .isEqualTo(Success(Person("b", "c", listOf("d"))))
+        ) { json, result ->
+            personOf(json) shouldBe Success(result)
+        }
     }
 
-    @Test
-    fun `JSON structure with null field is parsed correctly`() {
-        val myJson: Json = JsonObj(
-            mapOf(
-                "name" to JsonPrimitive("b"),
-                "hobbies" to JsonList(listOf(JsonPrimitive("d")))
+    "Object parser fails" {
+        forAll(
+            row(
+                JsonObj(mapOf("lastName" to JsonPrimitive("Schmidt"))),
+                listOf("<root>.firstName is mandatory but does not exist")
             )
-        )
-        Assertions.assertThat(personOf(myJson))
-            .isEqualTo(Success(Person("b", null, listOf("d"))))
+        ) { json, result ->
+            personOf(json).reasons().map { it.print() } shouldBe result
+        }
     }
 
-    @Test
-    fun `Missing hobbies field is detected`() {
-        val myJson: Json = JsonObj(
-            mapOf(
-                "name" to JsonPrimitive("b")
-            )
-        )
-        Assertions.assertThat(personOf(myJson))
-            .isEqualTo(Failure<Person>(listOf(Message("is mandatory but does not exist"))))
-    }
-
-    @Test
-    fun `Wrong hobbies field type is detected`() {
-        val myJson: Json = JsonObj(
-            mapOf(
-                "name" to JsonPrimitive("b"),
-                "hobbies" to JsonPrimitive("d")
-            )
-        )
-        Assertions.assertThat(personOf(myJson).reasons())
-            .isEqualTo(listOf(FieldReason("hobbies", Message("is not a list but a string"))))
-    }
-
-    @Test
-    fun `Hobbies field contains element of wrong type`() {
-        val myJson: Json = JsonObj(
-            mapOf(
-                "name" to JsonPrimitive("b"),
-                "hobbies" to JsonList(listOf(JsonPrimitive("d"), JsonList(listOf(JsonPrimitive("e")))))
-            )
-        )
-        Assertions.assertThat(personOf(myJson).reasons())
-            .isEqualTo(listOf(FieldReason("hobbies", IndexReason(1, Message("is not a string but a list")))))
-    }
-
-}
+})
