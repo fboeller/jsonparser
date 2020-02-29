36f146a839935170fffdc1a19443f4058c6ff7f5
fboeller <fabianboeller@googlemail.com>
2020-02-29 15:01:07 +0100

    Introduce Path structure

--- src/main/kotlin/io/fboeller/JsonTypes.kt
+++ src/main/kotlin/io/fboeller/JsonTypes.kt
@@ -5,6 +5,20 @@ data class JsonObj(val fields: Map<String, Json>) : Json()
 data class JsonList(val elements: List<Json>) : Json()
 data class JsonPrimitive(val value: String) : Json()
 
+// obj.list[3].name
+sealed class Path
+object PathRoot : Path()
+data class PathField(val field: String, val path: Path): Path()
+data class PathIndex(val index: Int, val path: Path): Path()
+
+fun Path.print(): String = when(this) {
+    is PathRoot -> ""
+    is PathField -> path.print() + "." + field
+    is PathIndex -> path.print() + "[" + index + "]"
+}
+
+val a = PathField("name", PathIndex(3, PathField("list", PathField("obj", PathRoot))))
+
 typealias Parser<T> = (Json) -> T
 typealias OParser<T> = (JsonObj) -> T
 typealias LParser<T> = (JsonList) -> T
