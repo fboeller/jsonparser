9425deb9cada2f1191b7eb2314d2474a8132937d
fboeller <fabianboeller@googlemail.com>
2020-03-01 23:21:49 +0100

    Make parsers own classes

--- src/main/kotlin/io/fboeller/ParserDSL.kt
+++ src/main/kotlin/io/fboeller/ParserDSL.kt
@@ -1,14 +1,14 @@
 package io.fboeller
 
 data class Parser<T>(val parse: (Json) -> Result<T>) {
-    fun list(): Parser<List<T>> = onlylist { list ->
+    fun list(): Parser<List<T>> = onlylist(LParser { list ->
         list.elements
             .map(this.parse)
             .mapIndexed { i, result -> result.mapFailures { IndexReason(i, it) } }
             .sequence()
-    }
+    })
 
-    fun field(name: String): OParser<T?> = { json ->
+    fun field(name: String): OParser<T?> = OParser { json ->
         json.fields[name]
             ?.let(this.parse)
             .sequence()
@@ -16,26 +16,26 @@ data class Parser<T>(val parse: (Json) -> Result<T>) {
     }
 }
 
-typealias OParser<T> = (JsonObj) -> Result<T>
-typealias LParser<T> = (JsonList) -> Result<T>
-typealias PParser<T> = (JsonPrimitive) -> Result<T>
+data class OParser<T>(val parse: (JsonObj) -> Result<T>)
+data class LParser<T>(val parse: (JsonList) -> Result<T>)
+data class PParser<T>(val parse: (JsonPrimitive) -> Result<T>)
 
 private fun <T> fold(fo: OParser<T>, fl: LParser<T>, fp: PParser<T>): Parser<T> = Parser { json ->
     when (json) {
-        is JsonObj -> fo(json)
-        is JsonList -> fl(json)
-        is JsonPrimitive -> fp(json)
+        is JsonObj -> fo.parse(json)
+        is JsonList -> fl.parse(json)
+        is JsonPrimitive -> fp.parse(json)
     }
 }
 
 private fun <T> failO(reason: String): OParser<T> =
-    { Failure(listOf(Message(reason))) }
+    OParser { Failure<T>(listOf(Message(reason))) }
 
 private fun <T> failL(reason: String): LParser<T> =
-    { Failure(listOf(Message(reason))) }
+    LParser { Failure<T>(listOf(Message(reason))) }
 
 private fun <T> failP(reason: String): PParser<T> =
-    { Failure(listOf(Message(reason))) }
+    PParser { Failure<T>(listOf(Message(reason))) }
 
 private fun <T> onlylist(parse: LParser<T>): Parser<T> =
     fold(failO("is not a list but an object"), parse, failP("is not a list but a string"))
@@ -48,7 +48,7 @@ private fun <T> string(parse: PParser<T>): Parser<T> =
 
 
 fun string(): Parser<String> =
-    string { Success(it.value) }
+    string (PParser { Success(it.value) })
 
 data class Fields2<T1, T2>(val p1: OParser<T1>, val p2: OParser<T2>) {
     fun <R> mapTo(f: ((T1, T2) -> R)): Parser<R> =
@@ -58,8 +58,8 @@ data class Fields2<T1, T2>(val p1: OParser<T1>, val p2: OParser<T2>) {
 fun <T1, T2> fields(p1: OParser<T1>, p2: OParser<T2>): Fields2<T1, T2> =
     Fields2(p1, p2)
 
-fun <T> OParser<T?>.mandatory(): OParser<T> = { json ->
-    this(json).flatMap { t ->
+fun <T> OParser<T?>.mandatory(): OParser<T> = OParser { json ->
+    this.parse(json).flatMap { t ->
         t?.let { Success(it) }
             ?: Failure<T>(listOf(Message("is mandatory but does not exist")))
     }
@@ -82,5 +82,5 @@ private fun <T1, T2, R> liftResult(f: (T1, T2) -> R): (Result<T1>, Result<T2>) -
 
 private fun <T1, T2, R> liftOParser(f: (T1, T2) -> R): (OParser<T1>, OParser<T2>) -> OParser<R> =
     { p1, p2 ->
-        { json -> liftResult(f)(p1(json), p2(json)) }
+        OParser { json -> liftResult(f)(p1.parse(json), p2.parse(json)) }
     }
